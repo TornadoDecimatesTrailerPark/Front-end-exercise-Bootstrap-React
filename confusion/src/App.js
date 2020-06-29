7c644aba2f17a4a6f4b1efbc4bc2fe2bdf2cb9d8
hy155 <hy155@students.waikato.ac.nz>
2020-06-29 16:05:56 +1200

    conponents part2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import{Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuConponent';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-     <Navbar dark color="primary">
-       <div className="container">
-         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-       </div>
-     </Navbar>
-     <Menu />
+import { DISHES } from './shared/dishes';
+class App extends Component {
+
+  constructor(props){
+  super(props);
+  this.state={
+    dishers:DISHES
+  };  
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
-  );
+    );
+  }
 }
 export default App;
