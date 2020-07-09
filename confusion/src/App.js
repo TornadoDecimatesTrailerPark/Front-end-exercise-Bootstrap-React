0584543dc25e83772a04a1d67465af5baabe684d
Chris <yuheming1993@gmail.com>
2020-07-09 12:35:28 +1200

    finish bug in assignment1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -6,11 +6,11 @@ import './App.css';
 import { DISHES } from './shared/dishes';
 class App extends Component {
 
-  constructor(props){
-  super(props);
-  this.state={
-    dishers:DISHES
-  };  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
   }
   render() {
     return (
--- confusion/src/components/MenuConponent.js
+++ confusion/src/components/MenuConponent.js
@@ -1,67 +1,45 @@
 import React, { Component } from 'react';
-import {
-    Card, CardImg, CardImgOverlay,
-    CardText, CardBody, CardTitle
-} from 'reactstrap';
-class Menu extends Component {
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle, CardText} from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
-    constructor(props) {
+class Menu extends Component{
+    constructor(props){
         super(props);
-
         this.state = {
-          selectedDish:null
-        }
-        console.log('Menu Component constructor is invoked');
-    }
-    componentDidMount(){
-        console.log('Menu Component constructor is invoked');
-    }
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
+            seleectedDish: null
+        };
+
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
+    onSelectedDish(dish){
+        this.setState({seleectedDish:dish});
     }
+
+    
     render() {
         const menu = this.props.dishes.map((dish) => {
-            return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
+            return(
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=> this.onSelectedDish(dish)}>                        
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />                
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
             );
         });
-        console.log('Menu Component constructor is invoked');
         return (
             <div className="container">
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                
+                <DishDetail dish={this.state.seleectedDish} />
+                
             </div>
-        );
+        )
     }
-} export default Menu;
+}
+
+export default Menu;
